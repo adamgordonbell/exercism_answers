846bd37e4393e7dd92c040cfb04747be017e77f8
Adam Bell <agbell@gmail.com>
2014-10-13 10:11:53 -0400

    clean up DNA

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -1,15 +1,14 @@
 module DNA (count, nucleotideCounts)
 where
 
-import qualified Control.Exception as E
 import qualified Data.Map as Map
 
 count :: Char -> String -> Int
-count char cs = foldr go 0 cs
+count char = foldr go 0
   where go c i
                | c == char = i+1
                | valid c = i
-               | otherwise = error ("invalid nucleotide " ++ (show c))
+               | otherwise = error ("invalid nucleotide " ++ show c)
 
 nucleotideCounts :: String -> Map.Map Char Int
 nucleotideCounts ns = Map.fromList [('A',count 'A' ns),('C', count 'C' ns),('G', count 'G' ns), ('T', count 'T' ns)]
