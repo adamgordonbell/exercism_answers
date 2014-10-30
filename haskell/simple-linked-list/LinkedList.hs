a80076c0b6a62f20514fdaa0a93819f8441cdfc3
Adam Bell <agbell@gmail.com>
2014-10-30 17:13:12 -0400

    outline of linked list function

---
+++ haskell/simple-linked-list/LinkedList.hs
@@ -0,0 +1,10 @@
+module LinkedList where
+
+isNil = undefined
+toList = undefined
+next = undefined
+datum = undefined
+nil = undefined
+fromList = undefined
+reverseLinkedList = undefined
+new = undefined
