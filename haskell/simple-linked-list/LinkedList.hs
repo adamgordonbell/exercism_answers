0852b3be4b94a9afa810ccc01721c249e5b48e67
Adam Bell <agbell@gmail.com>
2014-10-30 18:41:14 -0400

    working linked list

--- haskell/simple-linked-list/LinkedList.hs
+++ haskell/simple-linked-list/LinkedList.hs
@@ -1,10 +1,25 @@
 module LinkedList where
 
-isNil = undefined
-toList = undefined
-next = undefined
-datum = undefined
-nil = undefined
-fromList = undefined
-reverseLinkedList = undefined
-new = undefined
+data LinkedList a = Nil |
+                    LinkedList { datum ::  a, next :: LinkedList a }
+                    deriving Show
+
+nil :: LinkedList a
+nil = Nil
+
+isNil :: LinkedList a -> Bool
+isNil Nil = True
+isNil _ = False
+
+toList :: LinkedList a -> [a]
+toList Nil = []
+toList (LinkedList a n) = a : toList n
+
+fromList :: [a] -> LinkedList a
+fromList = foldr LinkedList Nil
+
+reverseLinkedList :: LinkedList a -> LinkedList a
+reverseLinkedList = fromList . reverse . toList
+
+new :: a -> LinkedList a -> LinkedList a
+new = LinkedList
