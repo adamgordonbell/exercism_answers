978fd6b054bf4599895328af0f357a46d6fa253e
Adam Bell <agbell@gmail.com>
2014-11-02 20:17:59 -0500

    working word count

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,6 +1,20 @@
-module WordCount
-    (
-    wordCount
-    ) where
+module WordCount (wordCount) where
 
-wordCount = undefined
+import Data.Map.Strict(Map, fromListWith)
+import Data.Char
+
+wordCount :: String -> Map String Int
+wordCount = fromListWith (+) . fmap tuple . splitWord
+
+splitWord :: String -> [String]
+splitWord = words . fmap toLower . replaceWhen f ' '
+  where f x = not (isAlphaNum x) && not (isSpace x)
+
+replaceWhen :: (a -> Bool) -> a -> [a] -> [a]
+replaceWhen f d = foldr go []
+  where go y ys
+          | f y = d:ys
+          | otherwise = y:ys
+
+tuple :: a -> (a,Int)
+tuple x = (x,1)
