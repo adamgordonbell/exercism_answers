1136a31ed57cc324d5d31f0e8e38b16a98243a84
Adam Bell <agbell@gmail.com>
2014-11-03 21:08:54 -0500

    cleaned up etl

--- haskell/etl/etl.hs
+++ haskell/etl/etl.hs
@@ -5,11 +5,11 @@ import qualified Data.Map.Strict as Map
 import Data.Char
 
 transform :: Map Int [String] -> Map String Int
-transform = Map.foldlWithKey' go Map.empty
+transform = Map.foldlWithKey' accumulateNewKeys Map.empty
 
-go :: Map String Int -> Int -> [String] -> Map String Int
-go m i ss = Map.union m . Map.fromList $ pair i ss
+accumulateTransforms :: Map String Int -> Int -> [String] -> Map String Int
+accumulateTransforms m i ss = Map.union m . Map.fromList $ pair i ss
 
 pair :: Int -> [String] -> [(String,Int)]
-pair i ss = zip lower (repeat i)
-  where lower = fmap (fmap toLower) ss
+pair i ss = zip (lower ss) (repeat i)
+  where lower = fmap . fmap toLower
